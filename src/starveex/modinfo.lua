b31c19126415899dcc276d83d99c55e2f3940564
xbony2 <xbony22@gmail.com>
2015-03-09 20:50:03 -0400

    very nice, very nice

---
+++ src/starveex/modinfo.lua
@@ -0,0 +1,10 @@
+-- This gives info about the mod. Don't change this without asking me.
+name = "StarveEx"
+description = "Adds various fun stuff."
+author = "Xbony2"
+version = "1.0.0a"
+
+api_version = 6
+
+dont_starve_compatible = true
+-- reign_of_giants_compatible = true -- Is disabled because it is untested (I currently do not own ROG)
